7c1683e937267eda011ee143a6e4260caa570ac7
apst147-lgtm <apst147@gmail.com>
2026-05-25 16:12:43 +0700

    fix: align project config with correct repo name wik

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/doraemon-sos-wiki/' : '/',
+  base: mode === 'production' ? '/doraemon-sos-wik/' : '/',
   plugins: [react()],
 }));
